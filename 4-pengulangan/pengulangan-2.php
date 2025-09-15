314631c447b1d7d728f2a225e4d4c94f85767b34
azayyy2129 <jujujuangsih88@gmail.com>
2025-09-15 13:30:19 +0700

    pengulangan

---
+++ 4-pengulangan/pengulangan-2.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Pengalangan 2</title>
+</head>
+<body>
+	<h1>pengulangan</h1>
+	<h2>for</h2>
+	<?php 
+  // $i =;
+  for ($i=1; $i <=10 ; $i++) { 
+  	echo "hello world <br>";
+  	
+  }
+
+ ?>
+ <h2>while</h2>
+	<?php 
+    $i = 6;
+    while ($i <= 5) {
+    	echo "hello world <br>";
+    	$i++;
+    }
+ ?>
+ <h2>do while</h2>
+<?php 
+  $i = 6;
+  do {
+  	echo "hallo world <br>";
+  	$i++;
+  }while ($i <= 5)
+
+ ?>
+
+</body>
+</html>
