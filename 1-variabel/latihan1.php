0d47c97cb3bea8d7949ad679fb06157c6bc6e00c
azayyy2129 <jujujuangsih88@gmail.com>
2025-09-15 13:28:03 +0700

    varibel

---
+++ 1-variabel/latihan1.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Variabel</title>
+</head>
+<body>
+   <?php 
+    $Nama="azay sidik";
+    
+     echo $Nama;
+
+    ?>
+</body>
+</html>
---
+++ 1-variabel/latihan2.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>variabel latihan 2</title>
+<body>
+</head>
+  <?php 
+   $nama="azay";
+   $kelas="11 RPl";
+   $alamat="sukahurip";
+
+   echo "Nama : $nama";
+   echo "<br>";
+   echo "Kelas : $kelas";
+   echo "<br>";
+     echo "alamat : $alamat";
+   echo "<br>";
+   ?>
+</body>
+</html>
